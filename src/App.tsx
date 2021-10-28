d25cc70469e9023b3f0a0ba7ee9a0c9deca2eb20
Leon <silenceace@gmail.com>
2021-10-28 09:51:49 +0800

    chore: demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { FaBeer } from 'react-icons/fa';
 import { Hello, Version, Image } from './components';
 import avatarImg from './assets/img/avatar.png';
 import txtExample from './assets/source/example.txt';
@@ -6,6 +9,15 @@ import hbsExample from './assets/source/example.hbs';
 export default () => (
   <div>
     <Image src={avatarImg} />
+    <Avatar size={64} icon={<UserOutlined />} />
+    <h3>
+      {' '}
+      Lets go for a
+      {' '}
+      <FaBeer />
+      ?
+      {' '}
+    </h3>
     <Version />
     <div>
       NODE_ENV:
